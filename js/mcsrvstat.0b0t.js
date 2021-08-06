1715d143098a0e8c5b2f19a878afc6c884aecd51
bierdosenhalter <529651+bierdosenhalter@users.noreply.github.com>
2021-08-06 15:27:59 +0200

    added a player list

--- js/mcsrvstat.0b0t.js
+++ js/mcsrvstat.0b0t.js
@@ -1,12 +1,21 @@
 const api = "https://api.mcsrvstat.us/2/0b0t.org";
 const divServer = document.getElementById("server");
+const divPlayers = document.getElementById("players");
 
 $(document).ready(function () {
     $.getJSON(api, function (json) {
         if (json.online === false) {
-            divServer.innerHTML = "<div style='color: #A9A9A9'>Server is offline!</div>";
+            divServer.innerHTML = "<div style='color: #A9A9A9'>Server is offline</div>";
         } else {
             divServer.innerHTML = "<div style='color: #33FF00'>" + json.players.online + " Players online</div>";
+
+            if (json.players.uuid !== null) {
+                jQuery.each(json.players.uuid, function (name, uuid) {
+                    divPlayers.innerHTML += "<div class='player'><img src='https://mc-heads.net/avatar/" + uuid + "/16' alt='playerhead'><a href='https://namemc.com/" + name + "' target='_blank'>" + name + "</a></div>"
+                });
+
+                divPlayers.classList.add("playersScrollbar");
+            }
         }
     });
 });
